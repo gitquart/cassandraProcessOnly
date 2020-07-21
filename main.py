3cb802d99afa74171ac306c8cfeb4100bae4ee5e
Ulyses Rico <ulyses@quart.com.mx>
2020-07-20 20:34:17 -0500

    ...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.query import SimpleStatement
+import writeFile as wf
 
 pathToHereWin='C:\\Users\\Acer\\Documents\\quart\\appsquart\\cassandraProcessOnly\\'
 
@@ -22,22 +23,50 @@ def update(period):
     session = cluster.connect()
     session.default_timeout=70
     print('Updating started...')
-    querySt="select book_number,subject,dt_publication_date from thesis.tbthesis where period_number="+str(period)+""
+    querySt="select book_number,subject,dt_publication_date,id_thesis from thesis.tbthesis where period_number="+str(period)+""
     statement = SimpleStatement(querySt, fetch_size=1000)
     for row in session.execute(statement):
+        id_thesis=''
         book_number=''
         subject=''
         date=''
-        book_number=row[0]  
-        subject=row[1]
+        #wf.appendInfoToFile(pathToHereWin,'5ta.txt',row[0])
+        id_thesis=str(row[3])
         date=row[2]
+        #Case for date and book_number
+        """
         if date!=date_null:
-            print(date)
+            book_number=row[0] 
+            #chunks=''
+        """        
+        subject=row[1]
+        chunks=''
+        chunks=subject.split(',')
+        if len(chunks)>1:
+            subject1=''
+            subject2=''
+            subject3=''
+            if len(chunks)==2:
+                subject1=chunks[0]
+                subject2=chunks[1]
+                querySubject=" subject_1='"+str(subject1).strip()+"',subject_2='"+str(subject2).strip()+"',multiple_subjects=True"
+                
+            if len(chunks)==3:
+                subject1=chunks[0]
+                subject2=chunks[1]
+                subject3=chunks[2]
+                querySubject=" subject_1='"+str(subject1).strip()+"',subject_2='"+str(subject2).strip()+"',subject_3='"+str(subject3).strip()+"',multiple_subjects=True"
+        else:
+            #Subject is 1 only
+            querySubject='multiple_subjects=False'        
+    
+        #Final update
+        updateSt='update thesis.tbthesis set '+querySubject+ ' where id_thesis='+id_thesis
+        print('ID:',id_thesis)
                       
-        #future = session.execute_async(updateSt)
-        #res= future.result();    
-        #print(idThesis,': updated')
-        #count=count+1
+        future = session.execute_async(updateSt)
+        res= future.result();    
+        
             
                               
     cluster.shutdown()          
